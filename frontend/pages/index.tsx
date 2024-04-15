6e4950642fb22514ffa7e5c14df3c5f94afc0ddb
Taka <takahito.uchino.1218@gmail.com>
2024-04-15 22:30:29 +0900

    [add]index.tsx追加
    index.tsxを編集しました。

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,10 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { GetServerSideProps, NextPage } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+const Home: NextPage<Props> = (props) => {
   return (
     <>
       <Head>
@@ -15,6 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <h1 className={styles.title}>{props.title}</h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -111,4 +117,14 @@ export default function Home() {
       </main>
     </>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {
+      title: "Hello, GraphQL!"
+    },
+  };
+};
+
+export default Home;
