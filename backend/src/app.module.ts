0f46c3a36dc115cdc45b12ba8653d5ff813540b6
Taka <takahito.uchino.1218@gmail.com>
2024-04-15 19:26:33 +0900

    [add]GraphQLサーバー作成
    GraphQLサーバーを作成し、起動することを確認しました。

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './components/posts/posts.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.join(
+        process.cwd(),
+        'src/generated/graphql/schema.gql',
+      ),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
