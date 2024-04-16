404bc02a936910aa3523b8fc8b6d189e5206aeea
Taka <takahito.uchino.1218@gmail.com>
2024-04-16 09:31:34 +0900

    [add]フロントエンド作成
    フロントエンドを作成し、バックエンドと疎通確認をしました。

--- frontend/src/libs/gql-requests.ts
+++ frontend/src/libs/gql-requests.ts
@@ -1,13 +1,15 @@
 import { initUrqlClient } from "next-urql";
-import { Client } from "urql";
+import { Client, ssrExchange, cacheExchange, fetchExchange } from "urql";
 
 const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT!;
 
 export function urqlClient(): Promise<Client> {
+  const ssrCache = ssrExchange({ isClient: false });
   return new Promise((resolve, reject) => {
     const client = initUrqlClient(
       {
         url: GRAPHQL_ENDPOINT,
+        exchanges: [cacheExchange, ssrCache, fetchExchange]
       },
       false,
     );
